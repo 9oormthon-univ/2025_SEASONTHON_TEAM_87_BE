baaac8064387ff7d4e06e91c6ac998a546bbb2e6
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-09-06 19:31:01 +0900

    test 코드 수정

--- src/test/java/com/developing/bluffing/MatchServiceImplTest.java
+++ src/test/java/com/developing/bluffing/MatchServiceImplTest.java
@@ -6,8 +6,6 @@ import com.developing.bluffing.game.service.MatchService;
 import com.developing.bluffing.user.entity.Users;
 import com.developing.bluffing.user.entity.enums.OauthProvider;
 import com.developing.bluffing.user.entity.enums.UserRole;
-import com.developing.bluffing.user.repository.UsersRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,58 +20,46 @@ public class MatchServiceImplTest {
     @Autowired
     private MatchService matchService;
 
-    @Autowired
-    private UsersRepository usersRepository;
-
-    @BeforeEach
-    void setUp() {
-        // 1. 매치 큐 초기화
-        matchService.clearQueue();
-
-        // 2. Users 테이블 초기화
-        usersRepository.deleteAll();
-    }
-
     @Test
     public void testEnqueueRegularMatch() {
-        // 6명의 테스트 유저 생성 및 DB 저장
-        Users user1 = usersRepository.save(Users.builder()
+        // 유저 생성
+        Users user1 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(2005, 1, 1))
-                .build());
+                .build();
 
-        Users user2 = usersRepository.save(Users.builder()
+        Users user2 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(2005, 2, 2))
-                .build());
+                .build();
 
-        Users user3 = usersRepository.save(Users.builder()
+        Users user3 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(2005, 3, 3))
-                .build());
+                .build();
 
-        Users user4 = usersRepository.save(Users.builder()
+        Users user4 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(2005, 4, 4))
-                .build());
+                .build();
 
-        Users user5 = usersRepository.save(Users.builder()
+        Users user5 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(2005, 5, 5))
-                .build());
+                .build();
 
-        Users user6 = usersRepository.save(Users.builder()
+        Users user6 = Users.builder()
                 .role(UserRole.USER)
                 .oauthType(OauthProvider.KAKAO)
                 .birth(LocalDate.of(1990, 6, 6))
-                .build());
+                .build();
 
-        // 유저 매치 등록
+        // 큐에 유저 등록
         matchService.enqueue(user1, MatchCategory.REGULAR);
         matchService.enqueue(user2, MatchCategory.REGULAR);
         matchService.enqueue(user3, MatchCategory.REGULAR);
@@ -81,16 +67,14 @@ public class MatchServiceImplTest {
         matchService.enqueue(user5, MatchCategory.REGULAR);
         matchService.enqueue(user6, MatchCategory.REGULAR);
 
-        System.out.println("6명의 유저가 큐에 정상 등록됨");
-
-        // 중복 등록 시 예외 발생 확인
+        // 이미 큐에 있는 유저를 다시 등록하면 예외 발생
         assertThrows(UserInGameInfoException.class, () -> {
             matchService.enqueue(user1, MatchCategory.REGULAR);
         });
-
-        System.out.println("중복 등록 시 UserInGameInfoException 발생 확인 완료");
     }
 }
+
+
 
 
 
